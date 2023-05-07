cc92f2d83770d627832a848b99909012b1363763
Rajul Jha <rajuljha49@gmail.com>
2023-05-08 00:24:51 +0530

    Update file

--- tan_to_sin_convertor.py
+++ tan_to_sin_convertor.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
+# readings
 tan_list_1 = [0.0013,0.0046,0.0093,0.0133,0.0166,0.0210,0.0253,0.0300,0.0333]
 tan_list_2 = [0.0060,0.0126,0.0183,0.0253,0.0316,0.0376,0.0433]
 tan_list_3 = [0.0030,0.0056,0.0090,0.0113,0.0150,0.0176,0.0210,0.0240,0.0263,0.0290]
 
+# convertion function
 def tan_to_sin(tan_theta):
     sin_theta = tan_theta / (1+tan_theta**2)**0.5
     return sin_theta
@@ -36,6 +38,7 @@ print(list_1)
 print(list_2)
 print(list_3)
 
+# plot the graph
 plt.plot(list_1, sin_list_1, label = "wire 1")
 plt.plot(list_2, sin_list_2, label = "wire 2")
 plt.plot(list_3, sin_list_3, label = "wire 3")
